99bdf4acd8373d764d64103921cc9ee15c15d7d2
Bob Silverberg <bob.silverberg@gmail.com>
2021-02-04 18:01:30 -0500

    Trying to retrieve site data in _app.js. Not possible with getServerSideProps

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,15 @@ import { UserProvider } from '../context/user';
 import 'styles/globals.css';
 import 'components/AddonBadges/styles.scss';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, siteData }) {
   // Here's a way to get config values into _app.js
   // const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
   // We can get params from the querystring
   const router = useRouter();
   const { clientApp, lang } = router.query;
+
+  console.log('----- siteData: ', siteData);
 
   // We can add page props to pass into every component.
   const props = { ...pageProps, clientApp, lang };
@@ -34,6 +36,15 @@ function MyApp({ Component, pageProps }) {
 MyApp.propTypes = {
   pageProps: PropTypes.shape({}),
   Component: PropTypes.elementType,
+};
+
+MyApp.getServerSideProps = async () => {
+  const res = await fetch(`	https://addons-dev.allizom.org/api/v5/site/`);
+  const statusCode = res.status > 200 ? res.status : false;
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { siteData: data, statusCode } };
 };
 
 export default MyApp;

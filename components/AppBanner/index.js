69d1dd68f4e5fff38795758625b31773daef3b28
Bob Silverberg <bob.silverberg@gmail.com>
2021-02-04 16:03:03 -0500

    Added AppBanner

---
+++ components/AppBanner/index.js
@@ -0,0 +1,19 @@
+import makeClassName from 'classnames';
+import PropTypes from 'prop-types';
+
+import SiteNotices from './SiteNotices';
+import SurveyNotice from './SurveyNotice';
+import styles from './styles.module.scss';
+
+export default function AppBanner({ className }) {
+  return (
+    <div className={makeClassName(styles.AppBanner, className)}>
+      <SiteNotices />
+      <SurveyNotice />
+    </div>
+  );
+}
+
+AppBanner.propTypes = {
+  className: PropTypes.string,
+};
